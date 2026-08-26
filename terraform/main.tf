9f4e1edbdd00fc8672fb26c4d594fafea1c0f42b
Rodrigo <rdgovieira@gmail.com>
2026-08-26 09:00:27 -0300

    fix(infra): add force_delete = true to ECR repository to allow terraform destroy with images

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_ecr_repository" "app_repo" {
   name                 = "p22-devops-ecs-fargate-repo-${terraform.workspace}"
   image_tag_mutability = "IMMUTABLE"
+  force_delete         = true
 }
 
 resource "aws_ecs_cluster" "main" {
